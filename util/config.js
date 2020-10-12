ff8d95f62677307691f172c7f5d61c4ab0e297d9
sparrow1116 <yingjiez1@126.com>
2020-10-12 21:53:25 +0800

    zyj commit for first version

--- util/config.js
+++ util/config.js
@@ -2,7 +2,7 @@
 const isServer = true;
 
  export default {
-	baseDemain:isServer ? 'http://47.103.124.76:3000' : 'https://192.168.1.5:3000',
+	baseDemain:isServer ? 'https://www.yangmaoba.club' : 'https://192.168.1.5:3000',
 	picDomain:isServer ? 'https://bankpictur.oss-cn-shanghai.aliyuncs.com' : 'http://192.168.10.7:7777',
 	bankIconDomain: 'https://wollpic.oss-cn-shanghai.aliyuncs.com/bank/'
 }
